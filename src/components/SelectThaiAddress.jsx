5d65fd5c36c13f7deca1ec3115f197e0d1280875
Thagoon Prakobsupan <thagoon.work@gmail.com>
2023-05-26 18:55:53 +0700

    add thai address lib

---
+++ src/components/SelectThaiAddress.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import {
+  searchAddressByDistrict,
+  searchAddressByAmphoe,
+  searchAddressByProvince,
+  searchAddressByZipcode,
+} from "thai-address-database";
+import PropTypes from "prop-types";
+import { Box } from "@mui/material";
+
+export default function SelectThaiAddress({ delimiter, label, name, value, setValue }) {
+  const [inputValue, setInputValue] = useState(value);
+  const finder = (searchBy, txt) => {
+    switch (searchBy) {
+      case "subdistrict":
+        return searchAddressByDistrict(txt).slice(0, 10);
+      case "district":
+        return searchAddressByAmphoe(txt).slice(0, 10);
+      case "province":
+        return searchAddressByProvince(txt).slice(0, 10);
+      case "zipcode":
+        return searchAddressByZipcode(txt).slice(0, 10);
+      default:
+        return [];
+    }
+  };
+
+  const handleSearch = (inputValue) => {
+    const foundData = finder(name, inputValue);
+    const mapArray = foundData.map((item, key) => {
+      return {
+        key: `${item.zipcode}${key}`,
+        title: `${item.district}${delimiter}${item.amphoe}${delimiter}${item.province}${delimiter}${item.zipcode}`,
+        optionLabel: item.district,
+      };
+    });
+    return mapArray;
+  };
+
+  const handleSelect = (value) => {
+    if (value) {
+      const addresses = value?.split(delimiter);
+      setValue("subdistrict", addresses[0]);
+      setValue("district", addresses[1]);
+      setValue("province", addresses[2]);
+      setValue("zipcode", addresses[3]);
+    }
+  };
+
+  return (
+    <Autocomplete
+      size="small"
+      isOptionEqualToValue={(option, value) => option === value}
+      getOptionLabel={(option) => option.optionLabel || value}
+      options={[]}
+      value={value}
+      onChange={(_, newValue) => {
+        handleSelect(newValue?.title);
+      }}
+      inputValue={inputValue}
+      onInputChange={(_, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
+      renderOption={(props, option) => (
+        <Box component="li" {...props}>
+          {option.title}
+        </Box>
+      )}
+      renderInput={(params) => {
+        return <TextField {...params} label={label} />;
+      }}
+      filterOptions={(_, { inputValue }) => handleSearch(inputValue)}
+    />
+  );
+}
+
+SelectThaiAddress.propTypes = {
+  delimiter: PropTypes.string,
+  label: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  setValue: PropTypes.func,
+};
--- src/pages/Settings/CompanyProfile.jsx
+++ src/pages/Settings/CompanyProfile.jsx
@@ -1,17 +1,140 @@
 import { useTranslation } from "react-i18next";
 import BoxHeader from "../../components/BoxHeader";
 import FilterlistBox from "../../components/FilterBox";
-import { Box } from "@mui/material";
+import { Box, Button, Grid, TextField } from "@mui/material";
 import AvatarUpload from "../../components/AvatarUpload";
+import { useForm, Controller } from "react-hook-form";
+import SelectThaiAddress from "../../components/SelectThaiAddress";
 
 const CompanyProfile = () => {
   const { t } = useTranslation();
+  const methods = useForm({
+    defaultValues: {
+      image: {},
+      name: "",
+      subdistrict: "",
+      district: "",
+      province: "",
+      zipcode: "",
+      birthday: {
+        date: "",
+        month: "",
+        year: "",
+      },
+      select: {},
+    },
+  });
+
+  const { control, setValue, handleSubmit } = methods;
+
+  const onSubmit = (formData) => console.log(formData);
 
   return (
     <Box sx={{ height: 1080, overflow: "auto" }}>
       <FilterlistBox noFilter={true} />
       <BoxHeader title={t("pages.setting.company.title")} noButton={true} />
       <AvatarUpload />
+      <Box p={2}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Grid container spacing={2}>
+            <Grid xs={12} item>
+              <Controller
+                name="name"
+                control={control}
+                render={({ field }) => <TextField size="small" label="Name" fullWidth disabled {...field} />}
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <Controller
+                name="name"
+                control={control}
+                render={({ field }) => <TextField size="small" label="Address #1" fullWidth disabled {...field} />}
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <Controller
+                name="name"
+                control={control}
+                render={({ field }) => <TextField size="small" label="Address #2" fullWidth disabled {...field} />}
+              />
+            </Grid>
+            <Grid xs={12} md={6} item>
+              <Controller
+                name="subdistrict"
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <SelectThaiAddress
+                      delimiter={","}
+                      label={"ตำบล"}
+                      name={field.name}
+                      value={field.value}
+                      setValue={setValue}
+                    />
+                  );
+                }}
+              />
+            </Grid>
+            <Grid xs={12} md={6} item>
+              <Controller
+                name="district"
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <SelectThaiAddress
+                      delimiter={","}
+                      label={"อำเภอ"}
+                      name={field.name}
+                      value={field.value}
+                      setValue={setValue}
+                    />
+                  );
+                }}
+              />
+            </Grid>
+            <Grid xs={12} md={6} item>
+              <Controller
+                name="province"
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <SelectThaiAddress
+                      delimiter={","}
+                      label={"จังหวัด"}
+                      name={field.name}
+                      value={field.value}
+                      setValue={setValue}
+                    />
+                  );
+                }}
+              />
+            </Grid>
+            <Grid xs={12} md={6} item>
+              <Controller
+                name="zipcode"
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <SelectThaiAddress
+                      delimiter={","}
+                      label={"รหัสไปรษณีย์"}
+                      name={field.name}
+                      value={field.value}
+                      setValue={setValue}
+                    />
+                  );
+                }}
+              />
+            </Grid>
+
+            <Grid xs={12} item>
+              <Button variant="contained" color="primary" type="submit" fullWidth>
+                Save
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
     </Box>
   );
 };
